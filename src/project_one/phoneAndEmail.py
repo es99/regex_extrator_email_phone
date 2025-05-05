619afdb72723a5b27a3ef6a19cef6cee84643a41
engels <engels.franca@icloud.com>
2025-05-04 23:58:00 -0300

    projeto concluido

--- src/project_one/phoneAndEmail.py
+++ src/project_one/phoneAndEmail.py
@@ -1,16 +1,17 @@
 """Projeto: extrator de números de telefone e endereços de email."""
 
+import pyperclip
 import re
 
 telRegex = re.compile(
     r"""
     (\d{2}|\(\d{2}\))?  # área
     (\s|-|\.)?          # separador
-    9?                  # separador dígito 9
+    (9?                  # separador dígito 9
     (\s|-|\.)?          # separador
     \d{4,5}             # primeira parte
     (\s|-|\.)?          # separador
-    \d{4}               # segunda parte
+    \d{4})               # segunda parte
 """,
     re.VERBOSE,
 )
@@ -24,3 +25,19 @@ emailRegex = re.compile(
 """,
     re.VERBOSE,
 )
+
+textTransferArea = str(pyperclip.paste())
+
+matches = []
+for groups in telRegex.findall(textTransferArea):
+    phoneNum = "".join([groups[0], groups[2]])
+    matches.append(phoneNum)
+for email in emailRegex.findall(textTransferArea):
+    matches.append(email)
+
+if len(matches) > 0:
+    pyperclip.copy("\n".join(matches))
+    print("Copied to Clipboard:")
+    print("\n".join(matches))
+else:
+    print("No phone numbers or email address found.")
